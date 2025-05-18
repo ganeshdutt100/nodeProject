353e26acf44d8fb06ef8ef6b9a5bd8e64ea80ad6
ganeshdutt <gdutt2304@gmail.com>
2025-05-18 15:45:07 +0530

    Update QR Code

---
+++ code2/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import qr from "qr-image";
+import cors from "cors";
+
+const PORT = 3000;
+const app = express();
+app.use(cors());
+
+app.get("/generate", (req, res) => {
+  const { url } = req.query;
+  console.log(url);
+
+  if (!url) return res.status(400).json({ error: "URL is required" });
+
+  const qr_png = qr.imageSync(url, { type: "png" });
+  const base64Image = `data:image/png;base64,${qr_png.toString("base64")}`;
+  res.json({ qr: base64Image });
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running at http://localhost:${PORT}`);
+});
--- qrGenerator/public/server.js
+++ qrGenerator/public/server.js
@@ -1,25 +1,22 @@
+// server.js
 import express from "express";
 import qr from "qr-image";
-import fs from "fs";
 import cors from "cors";
-import { error } from "console";
+
 const PORT = 3000;
-
 const app = express();
-
 app.use(cors());
-app.use(express.json());
 
-app.get("/generator", (req, res) => {
+app.get("/generate", (req, res) => {
   const { url } = req.query;
   if (!url) return res.status(400).json({ error: "URL is required" });
 
   const qr_png = qr.imageSync(url, { type: "png" });
-
   const base64Image = `data:image/png;base64,${qr_png.toString("base64")}`;
-  res.json({ qr_png: base64Image });
+
+  res.json({ qr: base64Image });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`✅ Server is running at http://localhost:${PORT}`);
 });
